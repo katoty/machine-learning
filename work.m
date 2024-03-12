c3b2e5b94df9f91a5e153f1c69cce36e40b6b7b0
lty-ytl <134470300+lty-ytl@users.noreply.github.com>
2024-03-12 09:26:56 +0800

    Update work.m

--- work.m
+++ work.m
@@ -1,5 +1,7 @@
 Data=xlsread("\data.xls");
+
 %获取行列数
+
 row=size(Data,1);
 column=size(Data,2);
 %最后一列加1.5
@@ -9,31 +11,38 @@ end
 tn=3000;
 
 %训练集
+
 train_xdata=Data(1:tn, 1: 7);
 train_ydata=Data(1:tn,8);
 %给训练集加一列1
+
 for i=1:tn
 train_xdata(i,8)=1;
 end
 
 %测试集
+
 test_xdata=Data(tn:row ,1 :7);
 test_ydata=Data(tn:row , 8);
 %给测试集加一列1
+
 for i=1:row-tn+1
 test_xdata(i,8)=1;
 end
 
 %训练启动
+
 X=train_xdata;
 Y=train_ydata;
 X_1=test_xdata;
 Y_1=test_ydata;
 w=inv(X'*X)*X'*Y;
 %计算预测值
+
 disp("预测值如下");
 predict_y=X_1*w
 %计算误差
+
 ebs=Y_1-predict_y;
 disp("均方误差如下");
 mse=norm(ebs,2)/(row-tn+1)
